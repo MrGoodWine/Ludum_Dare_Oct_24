91195e7e29d1d3c0ab704cbad7a5c93ec4c183ee
Alex Bezuska <abezuska@gmail.com>
2024-10-08 14:33:19 -0400

    Update alexs build script

--- build-rom.sh
+++ build-rom.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# Clean up previous build artifacts
 rm -rf Main.o
 rm -rf Main.obj
 rm -rf Main.smc
 rm -rf 'Super Bug Bash.smc'
+
+echo  "be sure sprites are updated"
+cd Pictures/
+./png2snes.sh
+cd ../
 
 # Create the temp.prj file
 echo "[objects]" > temp.prj
@@ -15,10 +21,21 @@ wla-65816 -o Main.o Main.asm
 # Link the compiled object file into a ROM
 wlalink -v temp.prj 'Super Bug Bash.smc'
 
-# Check if the -sd flag is passed
-if [[ "$1" == "-sd" ]]; then
-    cp 'Super Bug Bash.smc' /media/fufroom/Generic/SAG\ SNES\ Rom\ Pack/
-    echo "ROM Built and added to SD Card"
-else
-    echo "ROM Built. Use -sd flag to copy to the SD card."
-fi
+# Check for flags
+for arg in "$@"; do
+  case $arg in
+    -sd)
+      # Copy to SD card
+      cp 'Super Bug Bash.smc' /media/fufroom/Generic/SAG\ SNES\ Rom\ Pack/
+      echo "ROM Built and added to SD Card"
+      ;;
+    -o)
+      # Open ROM in Mesen
+      ~/src/Mesen 'Super Bug Bash.smc' &
+      echo "ROM opened in Mesen"
+      ;;
+    *)
+      echo "ROM Built. Use -sd flag to copy to the SD card or -o to open in Mesen."
+      ;;
+  esac
+done
